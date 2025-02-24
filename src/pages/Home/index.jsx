47031b2d0b3d0f3825ab90f17779a68a02bf51d3
Indy Erens <indyerens@macbookpro.home>
2025-02-24 15:50:32 +0100

    changed the profile picture, added different colors

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,8 @@
 function Home (){
   return (
     <div className="text-center p-10">
-      <h1 className="text-4xl font-bold">Tabula</h1>
-      <p className="text-gray-600">This is the home page content.</p>
+      <h1 className="text-8xl font-bold ">Tabula</h1>
+      <p className="text-gray-600 font-bold">Home</p>
     </div>
   );
 };
