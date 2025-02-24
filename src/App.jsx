72c56738cd8b9055558f7e5d6c0f7d07d779ca2e
Evangelia Tosiou <tosiouevangelia@gmail.com>
2025-02-24 16:05:33 +0100

    Pages with details for each location!

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer"
 import About from "./pages/About";
 import SignUp from "./components/SignUp";
+import LocationDetails from "./pages/LocationDetails";
 
 
 function Layout() {
@@ -25,7 +26,8 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
-        <Route path="signUp" element={<SignUp/>}/>        
+        <Route path="signUp" element={<SignUp/>}/> 
+        <Route path="/location/:id" element={<LocationDetails />} />  
       </Route>
     </Routes>
   );
